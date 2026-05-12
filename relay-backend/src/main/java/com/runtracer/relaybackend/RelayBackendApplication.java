609fadb1fb2d11630a1f1e1a1d7800a14ea5a6e6
Christian Burger <christian_burger@yahoo.com>
2026-05-12 12:47:26 -0300

    initial backend.

---
+++ relay-backend/src/main/java/com/runtracer/relaybackend/RelayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.runtracer.relaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayBackendApplication.class, args);
+	}
+
+}
---
+++ relay-backend/src/test/java/com/runtracer/relaybackend/RelayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runtracer.relaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
